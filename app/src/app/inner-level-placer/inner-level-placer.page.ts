081f37a85dd46f66c1fead476b576dd1263e47aa
Nidhoggr <Raxex@users.noreply.github.com>
2019-10-05 00:56:20 -0300

    cambios en vista

--- app/src/app/inner-level-placer/inner-level-placer.page.ts
+++ app/src/app/inner-level-placer/inner-level-placer.page.ts
@@ -11,7 +11,27 @@ import { ToastController } from '@ionic/angular';
 export class InnerLevelPlacerPage implements OnInit {
 
   constructor(public conn:ConectorService,private router: Router,public toastController: ToastController) { }
+  
+  isenabledT1:false //Tutorial 2
+  isenabledT2:false //Tutorial 3
+  isenabledT3:false //Tutorial 4
+  isenabledT4:false //Tutorial 5
 
+  isenabledA1:false //Nivel 1 - Ejercicio 2
+  isenabledA2:false //Nivel 1 - Ejercicio 3
+  isenabledA3:false //Nivel 1 - Ejercicio 4
+  isenabledA4:false //Nivel 1 - Ejercicio 5
+
+  isenabledB1:false //Nivel 2 - Ejercicio 2
+  isenabledB2:false //Nivel 2 - Ejercicio 3
+  isenabledB3:false //Nivel 2 - Ejercicio 4
+  isenabledB4:false //Nivel 2 - Ejercicio 5
+  
+  isenabledC1:false //Nivel 3 - Ejercicio 2
+  isenabledC2:false //Nivel 3 - Ejercicio 
+  isenabledC3:false //Nivel 3 - Ejercicio 4
+  isenabledC4:false //Nivel 3 - Ejercicio 5
+  
   ngOnInit() {
   }
 
